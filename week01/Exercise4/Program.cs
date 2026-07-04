19d7900b970aecfbb11e0e2d6b9fc29de9f3e8e4
Collins Zivanai <czivanai@byupathway.edu>
2026-07-04 17:30:38 +0200

    Initial commit

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+         List<int> numbers = new List<int>();
+        
+        int userInput = -1;
+        while (userInput != 0)
+        {
+            Console.Write("Enter any number and enter 0 to quit: ");
+            
+            string Response = Console.ReadLine();
+            userInput = int.Parse(Response);
+            
+            if (userInput != 0)
+            {
+                numbers.Add(userInput);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+                float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
     }
 }
+    
